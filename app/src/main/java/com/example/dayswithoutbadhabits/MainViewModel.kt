0a21d21c1b3f4c44c7fc1a0c56dc3b4fc14cd54d
andrew <andrew.maneshin@gmail.com>
2023-01-29 17:12:59 +0300

    Rebase classes

--- app/src/main/java/com/example/dayswithoutbadhabits/MainViewModel.kt
+++ app/src/main/java/com/example/dayswithoutbadhabits/MainViewModel.kt
@@ -1,8 +1,5 @@
 package com.example.dayswithoutbadhabits
 
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.lifecycle.*
 
 class MainViewModel(
@@ -29,53 +26,4 @@ class MainViewModel(
 
     override fun observe(owner: LifecycleOwner, observer: Observer<UiState>) =
         communication.observe(owner, observer)
-}
-
-sealed class UiState {
-
-    abstract fun apply(daysTextView: TextView, resetButton: Button)
-
-    object ZeroDays : UiState() {
-        override fun apply(daysTextView: TextView, resetButton: Button) {
-            daysTextView.text = "0"
-            resetButton.visibility = View.GONE
-        }
-    }
-
-    data class NDays(private val days: Int) : UiState() {
-        override fun apply(daysTextView: TextView, resetButton: Button) {
-            daysTextView.text = days.toString()
-            resetButton.visibility = View.VISIBLE
-        }
-    }
-}
-
-interface MainCommunication {
-
-    interface Put {
-        fun put(value: UiState)
-    }
-
-    interface Observe {
-        fun observe(owner: LifecycleOwner, observer: Observer<UiState>)
-    }
-
-    interface Mutable : Put, Observe
-
-    class Base(private val liveData: MutableLiveData<UiState>) : Mutable {
-
-        override fun put(value: UiState) {
-            liveData.value = value
-        }
-
-        override fun observe(owner: LifecycleOwner, observer: Observer<UiState>) {
-            liveData.observe(owner, observer)
-        }
-    }
-}
-
-interface MainRepository {
-
-    fun days(): Int
-    fun reset()
 }
